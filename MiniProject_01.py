3d3064d51ce964cd816766fa85bd7efe9e4b2f40
faizih111 <eng.faizan11@gmail.com>
2025-08-13 09:23:52 +0500

    Create MiniProject_01.py
    Add Favorite Book Info Card project

---
+++ MiniProject_01.py
@@ -0,0 +1,7 @@
+BookTitle,AuthorName =str("AI Engineering"),str(" Chip Huyen")
+NumberOfPages,PriceInDollars=int(109),float(57.0)
+print("___Favorite Book Info Card___")
+print("Book Title :",BookTitle)
+print("Author Name :",AuthorName)
+print("Number of Pages :",NumberOfPages)
+print("Price in Dollars :",PriceInDollars)
